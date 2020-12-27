f913209fb0c3562eb07261384bdbd493b2345ea2
Gabriellji <gabriellji@yahoo.com>
2020-12-27 21:44:13 +0300

    feat: start the server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
+const chalk = require('chalk')
+
 export const app = express()
 
 app.disable('x-powered-by')
@@ -12,4 +14,16 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+app.post('/', (req, res) => {
+  console.log(chalk.yellow.bold(req.body))
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log(chalk.blue.bold('server is on 3000'))
+  })
+}
