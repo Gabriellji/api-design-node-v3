c6ac086901bcb1a54af2c4af1d06c130365b0908
Gabriellji <gabriellji@yahoo.com>
2020-12-28 18:04:32 +0300

    feat: test CRUD controller

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,33 @@
-import { crudControllers } from '../../utils/crud'
+//import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
+import mongoose from 'mongoose';
 
-export default crudControllers(Item)
+import { connect } from '../../utils/db';
+
+const run = async () => {
+    await connect('mongodb://localhost:27017/api-test')
+
+    const item = await Item.create({
+        name: 'clean up',
+        createdBy: mongoose.Types.ObjectId(),
+        list: mongoose.Types.ObjectId()
+    })
+
+    console.log(await (await Item.find({})))
+
+
+    const updated = await Item.findByIdAndUpdate(
+        item._id, 
+        { name: 'eat' }, 
+        { new: true }
+        )
+    console.log(updated)
+
+
+    const removed = await Item.findByIdAndRemove(item._id)
+    console.log(removed)
+}
+
+run()
+
+//export default crudControllers(Item)
